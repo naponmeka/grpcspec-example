3898748773169c4202e8acd48641a8fc75ddb8d4
Napon Mekavuthikul <napon.meka@gmail.com>
2019-07-04 12:16:29 +0700

    Automate python gen init script

--- genpyinit.sh
+++ genpyinit.sh
@@ -1,15 +1,19 @@
-# cp generated/py/grpcspec/*.py pybrijgrpc/
 cat <<EOF >pygrpcspec/__init__.py
 from . import proto
 __all__ = [
 	'proto'
 ]
 EOF
-cat <<EOF >pygrpcspec/proto/__init__.py
-from . import todo_pb2_grpc
-from . import todo_pb2
-__all__ = [
-	'todo_pb2_grpc',
-	'todo_pb2'
-]
-EOF
+pyfiles=($(ls pygrpcspec/proto | sed -e 's/\..*$//'| grep -v __init__))
+rm -f pygrpcspec/proto/__init__.py
+for i in "${pyfiles[@]}"
+do
+	echo "from . import $i" >> pygrpcspec/proto/__init__.py
+done
+
+echo "__all__ = [" >> pygrpcspec/proto/__init__.py
+for i in "${pyfiles[@]}"
+do
+	echo "    '$i'," >> pygrpcspec/proto/__init__.py
+done
+echo "]" >> pygrpcspec/proto/__init__.py
--- pygrpcspec/proto/__init__.py
+++ pygrpcspec/proto/__init__.py
@@ -1,6 +1,6 @@
-from . import todo_pb2_grpc
 from . import todo_pb2
+from . import todo_pb2_grpc
 __all__ = [
-	'todo_pb2_grpc',
-	'todo_pb2'
+    'todo_pb2',
+    'todo_pb2_grpc',
 ]
